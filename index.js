fb7f4408e67e54aab3ef7f539593b1593f416134
YesicAr-c <129418938+YesicAr-c@users.noreply.github.com>
2023-03-30 14:02:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+type="text/javascript">
+    
+    window.addEventListener("scroll", function(){
+      var header = document.querySelector("header");
+      header.classList.toggle('down', window.scrollY > 0);
+	    
+	    var logo = document.querySelector(".logo img");
+		if (window.scrollY>0) {
+			logo.setAttribute('src', 'logo_verde.jpeg');
+		}else{
+			logo.setAttribute('src', 'logo_verde.jpeg');
+		}
+
+    });
+
+    var menu = document.querySelector('.menu');
+    var menuBtn = document.querySelector('.menu-btn');
+    var closeBtn = document.querySelector('.close-btn');
+
+    menuBtn.addEventListener("click", () => {
+      menu.classList.add('active');
+    });
+
+    closeBtn.addEventListener("click", () => {
+      menu.classList.remove('active');
+    });
